f7ff6de91fa216eb5ad76bc9dac66ff01e2a0b49
Huiying Wang <36682988+HuiyingWang0108@users.noreply.github.com>
2022-04-12 20:24:00 -0700

    Update dynamic.py

--- dynamic.py
+++ dynamic.py
@@ -1,51 +1,49 @@
-from traceback import print_tb
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
-  
-#url of the page we want to scrape
-url = "https://www.volkswagenag.com/en/group/portrait-and-production-plants.html"
-# Request URL: https://www.volkswagenag.com/content/dam/online-kommunikation/brands/corporate/world/presence/konzern/images/locations/europe/Schweden%20-%20Oskarshamn%20-%20Scania.jpg
-# Request URL: https://www.volkswagenag.com/content/dam/online-kommunikation/brands/corporate/world/presence/konzern/images/locations/asia/Taiwan%20-%20Ping%20Chen%20City%20-%20Scania.jpg
-# Request URL: https://www.volkswagenag.com/content/dam/online-kommunikation/brands/corporate/world/presence/konzern/images/locations/asia/Thailand%20-%20Amphur%20Pluakdaeng%20Rayong%20-%20Ducati.jpg
-# Request URL: https://www.volkswagenag.com/content/dam/online-kommunikation/brands/corporate/world/presence/konzern/images/locations/northamerica/Tulsa-Navistar.jpg
-# Request URL: https://www.volkswagenag.com/content/dam/online-kommunikation/brands/corporate/world/presence/konzern/images/locations/northamerica/USA%20-%20Chattanooga%20-%20VW.jpg
-# Request URL: https://www.volkswagenag.com/content/dam/online-kommunikation/brands/corporate/world/presence/konzern/images/locations/northamerica/Springfield-Navistar.jpg
+import os
 
-# Request URL: https://www.volkswagenag.com/content/dam/online-kommunikation/brands/corporate/world/presence/konzern/images/locations/northamerica/Mexiko%20-%20Puebla%20-%20VW.jpg
-# Request URL: https://www.volkswagenag.com/content/dam/online-kommunikation/brands/corporate/world/presence/konzern/images/locations/northamerica/Mexiko%20-%20Puebla%20-%20VW.jpg
+# print(dir(selenium))
 
-  
+# url of the page we want to scrape
+url = "https://global.toyota/en/company/profile/facilities/manufacturing-worldwide/"
 # initiating the webdriver. Parameter includes the path of the webdriver.
-driver = webdriver.Chrome(r'C:\Users\v-huiywang\webdriver\chromedriver') 
+s = Service(r"C:\Users\v-huiywang\webdriver\chromedriver.exe")
+driver = webdriver.Chrome(service = s)
+print(type(driver))
 driver.get(url) 
-  
-# this is just to ensure that the page is loaded
-time.sleep(5) 
-  
-html = driver.page_source
-  
-# this renders the JS code and stores all
-# of the information in static HTML code.
-  
-# Now, we could simply apply bs4 to html variable
-soup = BeautifulSoup(html, "html.parser")
-all_divs = soup.find('div', {'id' : 'map-container-v1-overlay'})
-# all_divs = soup.select('#map-container-v1-overlay')
-# print(all_divs)
-plant_names = all_divs.find_all("h4", {"class": "js-maps-headline"})
 
-for plant_name in plant_names:
-    name = plant_name.text
-    print(name)
-# # printing top ten job profiles
-# count = 0
-# for job_profile in job_profiles :
-#     print(job_profile.text)
-#     count = count + 1
-#     if(count == 10) :
-#         break
-  
-# driver.close() # closing the webdriver
+list_url = []
+# Locate the control.
+buttons = driver.find_elements(by = By.CLASS_NAME, value = "btn_text")
+for btn in buttons:
+    list_url.append(btn.get_attribute("href"))
+for url in list_url:
+    driver.get(url)
+    res = requests.get(url)
+    if res.status_code != 200:
+        print("Error fetching page")
+        exit()
+    else:
+        try:
+            soup = BeautifulSoup(res.text, 'html.parser')
+            table = soup.select('.newsTBL')
+            # table = soup.select('#mw-content-text > div.mw-parser-output > table:nth-child(7)')
+            df = pd.read_html(str(table))
+
+            # convert list to dataframe
+            df = pd.DataFrame(df[0])
+            # print(df.head())
+            # Export to csv
+            os.makedirs("toyota", exist_ok=True)
+            df.to_csv("toyota/{0}_toyota.csv".format(url.split("/")[-1].replace(".html", "")), index = False)
+        except ValueError as err:
+            print(url + " returned error: {0}".format(err))
